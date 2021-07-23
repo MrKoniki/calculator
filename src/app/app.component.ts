f1a34f1e66c4dcaa193a432ff88201fba45d4ec3
Korneichuk Nikita <korneichuk.nikita@gmail.com>
2021-07-23 18:54:45 +0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  public answerObj = {
+    answer: 0,
+    correctAnswer: 20,
+  };
+
+  async calc() {
+    this.answerObj.answer = parseInt(
+      (<HTMLInputElement>document.getElementById('value')).value
+    );
+    return this.answerObj.answer === this.answerObj.correctAnswer
+      ? alert('Right!')
+      : alert('Wrong!');
+  }
+
   title = 'calculator';
 }
